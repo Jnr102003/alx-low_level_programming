ba5876753b7f5cd858224abb7218a976d872b53e
Jnr102003 <jabezackom@gmail.com>
2022-10-13 04:49:37 -0700

    i

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+/**
+
+ * main - Starting point
+
+ *
+
+ * Description: Displays "with proper grammar, but the outcome is a piece of
+
+ * art,"
+
+ *
+
+ * Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
+
