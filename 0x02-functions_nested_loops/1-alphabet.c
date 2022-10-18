e03f78b1e6e78c596bd3a39404cad3e1e0158b58
Jnr102003 <52883414+Jnr102003@users.noreply.github.com>
2022-10-18 12:11:53 +0000

    Create 1-alphabet.c
    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet - A function that prints the alphabets in lowercase.
+*
+*Return: Always 0(Success)
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
